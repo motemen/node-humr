382792438c6a5ac973bee30dbd96e6836be2a005
motemen <motemen@gmail.com>
2015-12-09 19:34:43 +0900

    parsers

--- src/parser.ts
+++ src/parser.ts
@@ -18,5 +18,25 @@ export class DelimiterParser implements Parser {
   }
 }
 
+export class WholeParser implements Parser {
+  parse(line: string): string[] {
+    return [line];
+  }
+}
+
+export class LTSVParser implements Parser {
+  parse(line: string): string[] {
+    let parts = `\t${line}`.split(/(\t[0-9A-Za-z_.-]+:)/);
+    if (parts.length > 1) {
+      parts[1] = parts[1].replace(/^\t/, '');
+    } else {
+      parts[0] = parts[0].replace(/^\t/, '');
+    }
+    return parts;
+  }
+}
+
 registry.register('delimiter', DelimiterParser);
 registry.register('regexp',    DelimiterParser);
+registry.register('line',      WholeParser);
+registry.register('ltsv',      LTSVParser);
